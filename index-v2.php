91704a31bfdd64147cd96c0f00ad2de5fa006535
rnagarajan <ramesh@billionapps.net>
2020-04-02 20:37:50 +0530

    title change lowercase

--- index-v2.php
+++ index-v2.php
@@ -3,7 +3,7 @@
    <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>BillionApps - Home</title>
+      <title>Billionapps - Home</title>
       <!-- fonts -->
       <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700%7COswald:300,400,500,600,700" rel="stylesheet" type="text/css">
       <!-- styles -->   
